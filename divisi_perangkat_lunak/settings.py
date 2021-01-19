4efce4971f59f75f2a9d042138a1f5eb829a41ae
alviandk <alviandk@gmail.com>
2021-01-19 21:59:40 +0700

    user models and auths

--- divisi_perangkat_lunak/settings.py
+++ divisi_perangkat_lunak/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'auth',
+    'auths',
     'blog',
     'course',
     'enrollment',
@@ -126,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'user.DplUser'
